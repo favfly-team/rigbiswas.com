3d8bcfa27ff1f640ac917616a416fec54c08f87a
Darpan Chakraborty <favfly.darpan@gmail.com>
2023-02-08 12:03:22 +0530

    update

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,81 +1,81 @@
-import { useEffect } from 'react';
-import lozad from 'lozad';
-import Prismic from '@prismicio/client';
-import { Client } from '../utils/prismicHelpers';
-import { queryRepeatableDocuments } from '../utils/queries';
-import { SliceZone } from '../slices';
-import SEO from '../components/seo/SEO';
+import { useEffect } from "react";
+import lozad from "lozad";
+import Prismic from "@prismicio/client";
+import { Client } from "../utils/prismicHelpers";
+// import { queryRepeatableDocuments } from "../utils/queries";
+import { SliceZone } from "../slices";
+import SEO from "../components/seo/SEO";
 
-import BlogsSection from '../components/blog/BlogsSection';
+import BlogsSection from "../components/blog/BlogsSection";
 
 const ServicePage = ({ doc, blogPosts }) => {
-	// console.log(doc);
-	// ========== LOZAD INSTANTIATE ==========
-	useEffect(() => {
-		const observer = lozad('.lozad', {
-			rootMargin: '100px 0px', // syntax similar to that of CSS Margin
-		});
-		observer.observe();
-		return () => {};
-	}, [doc?.uid]);
-	// ========== END ==========
-	return (
-		<>
-			<SEO doc={doc} url={`https://rigbiswas.com/${doc?.uid}`} />
-			<SliceZone sliceZone={doc.data.body} />
-			<BlogsSection posts={blogPosts} />
-		</>
-	);
+  // console.log(doc);
+  // ========== LOZAD INSTANTIATE ==========
+  useEffect(() => {
+    const observer = lozad(".lozad", {
+      rootMargin: "100px 0px", // syntax similar to that of CSS Margin
+    });
+    observer.observe();
+    return () => {};
+  }, [doc?.uid]);
+  // ========== END ==========
+  return (
+    <>
+      <SEO doc={doc} url={`https://rigbiswas.com/${doc?.uid}`} />
+      <SliceZone sliceZone={doc.data.body} />
+      <BlogsSection posts={blogPosts} />
+    </>
+  );
 };
 
-export async function getStaticPaths() {
-	const documents = await queryRepeatableDocuments(
-		(doc) => doc.type === 'service_page'
-	);
-	return {
-		paths: documents.map((doc) => {
-			return { params: { slug: doc.uid } };
-		}),
-		fallback: 'blocking',
-	};
-}
+// export async function getStaticPaths() {
+//   const documents = await queryRepeatableDocuments(
+//     (doc) => doc.type === "service_page"
+//   );
+//   return {
+//     paths: documents.map((doc) => {
+//       return { params: { slug: doc.uid } };
+//     }),
+//     fallback: "blocking",
+//   };
+// }
 
-export async function getStaticProps({
-	preview = null,
-	previewData = {},
-	params,
+export async function getServerSideProps({
+  preview = null,
+  previewData = {},
+  params,
 }) {
-	const { ref } = previewData;
+  const { ref } = previewData;
 
-	const client = Client();
+  const client = Client();
 
-	const doc =
-		(await client.getByUID(
-			'service_page',
-			params.slug,
-			ref ? { ref } : null
-		)) || {};
+  const doc =
+    (await client.getByUID(
+      "service_page",
+      params.slug,
+      ref ? { ref } : null
+    )) || {};
 
-	const blogPosts = await client.query(
-		Prismic.Predicates.at('document.type', 'blog_post'),
-		{ pageSize: 6, orderings: '[my.blog_post.published_date desc]' }
-	);
+  const blogPosts = await client.query(
+    Prismic.Predicates.at("document.type", "blog_post"),
+    { pageSize: 6, orderings: "[my.blog_post.published_date desc]" }
+  );
 
-	if (doc.id == undefined) {
-		return {
-			props: null,
-			notFound: true,
-		};
-	}
+  if (doc.id == undefined) {
+    return {
+      props: null,
+      notFound: true,
+    };
+  }
 
-	return {
-		props: {
-			doc,
-			blogPosts,
-			preview,
-		},
-		revalidate: 60,
-	};
+  return {
+    props: {
+      doc,
+      blogPosts,
+      preview,
+    },
+    // revalidate: 60,
+  };
 }
 
 export default ServicePage;
--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,57 @@
-import { useEffect } from 'react';
-import lozad from 'lozad';
-import Prismic from '@prismicio/client';
-import { Client } from '../utils/prismicHelpers';
-import { SliceZone } from '../slices';
-import SEO from '../components/seo/SEO';
+import { useEffect } from "react";
+import lozad from "lozad";
+import Prismic from "@prismicio/client";
+import { Client } from "../utils/prismicHelpers";
+import { SliceZone } from "../slices";
+import SEO from "../components/seo/SEO";
 
-import BlogsSection from '../components/blog/BlogsSection';
+import BlogsSection from "../components/blog/BlogsSection";
 
 const IndexPage = ({ doc, blogPosts, posts }) => {
-	// console.log(blogPosts);
-	// ========== LOZAD INSTANTIATE ==========
-	useEffect(() => {
-		const observer = lozad('.lozad', {
-			rootMargin: '100px 0px', // syntax similar to that of CSS Margin
-		});
-		observer.observe();
-		return () => {};
-	}, [doc?.uid]);
-	// ========== END ==========
-	return (
-		<>
-			<SEO doc={doc} url='https://rigbiswas.com' />
-			<SliceZone sliceZone={doc.data.body} />
-			<BlogsSection posts={blogPosts} />
-		</>
-	);
+  // console.log(blogPosts);
+  // ========== LOZAD INSTANTIATE ==========
+  useEffect(() => {
+    const observer = lozad(".lozad", {
+      rootMargin: "100px 0px", // syntax similar to that of CSS Margin
+    });
+    observer.observe();
+    return () => {};
+  }, [doc?.uid]);
+  // ========== END ==========
+  return (
+    <>
+      <SEO doc={doc} url="https://rigbiswas.com" />
+      <SliceZone sliceZone={doc.data.body} />
+      <BlogsSection posts={blogPosts} />
+    </>
+  );
 };
 
-export async function getStaticProps({ preview = null, previewData = {} }) {
-	const { ref } = previewData;
-	const client = Client();
-	const doc = (await client.getSingle('home_page', ref ? { ref } : null)) || {};
+export async function getServerSideProps({ preview = null, previewData = {} }) {
+  const { ref } = previewData;
+  const client = Client();
+  const doc = (await client.getSingle("home_page", ref ? { ref } : null)) || {};
 
-	const blogPosts = await client.query(
-		Prismic.Predicates.at('document.type', 'blog_post'),
-		{ pageSize: 6, orderings: '[my.blog_post.published_date desc]' }
-	);
+  const blogPosts = await client.query(
+    Prismic.Predicates.at("document.type", "blog_post"),
+    { pageSize: 6, orderings: "[my.blog_post.published_date desc]" }
+  );
 
-	if (doc.id == undefined) {
-		return {
-			props: null,
-			notFound: true,
-		};
-	}
+  if (doc.id == undefined) {
+    return {
+      props: null,
+      notFound: true,
+    };
+  }
 
-	return {
-		props: {
-			doc,
-			blogPosts,
-			preview,
-		},
-		revalidate: 60,
-	};
+  return {
+    props: {
+      doc,
+      blogPosts,
+      preview,
+    },
+    // revalidate: 60,
+  };
 }
 
 export default IndexPage;
