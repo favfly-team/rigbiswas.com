7bd036052eb87c06aa9b05844a671a31ba889d7c
Darpan Chakraborty <favfly.darpan@gmail.com>
2023-04-29 11:23:14 +0530

    update

--- components/blog/BlogPostSection.js
+++ components/blog/BlogPostSection.js
@@ -1,68 +1,68 @@
 /* eslint-disable @next/next/no-img-element */
-import { useState, useEffect } from 'react';
-import { RichText } from 'prismic-reactjs';
-import { linkResolver } from '../../prismic-configuration';
-import dayjs from 'dayjs';
+import { useState, useEffect } from "react";
+import { RichText } from "prismic-reactjs";
+import { linkResolver } from "../../prismic-configuration";
+import dayjs from "dayjs";
 dayjs().format();
-import countapi from 'countapi-js';
+// import countapi from 'countapi-js';
 
 const BlogPostSection = ({ data, uid }) => {
-	const {
-		title,
-		description,
-		featured_image,
-		published_date,
-		blog_post_content,
-	} = data;
+  const {
+    title,
+    description,
+    featured_image,
+    published_date,
+    blog_post_content,
+  } = data;
 
-	/* ===== COUNT & UPDATE NO. OF VIEWS ===== */
-	const [views, setViews] = useState(0);
-	useEffect(() => {
-		countapi.hit('rigbiswas.com', uid).then((result) => {
-			result?.value && setViews(result.value);
-		});
-	}, [uid]);
-	/* ===== END ===== */
-	return (
-		<section>
-			<div className='wrapper light-wrapper'>
-				<div className='container inner pt-60'>
-					<div className='row'>
-						<div className='col-md-8 mx-auto'>
-							<div className='blog classic-view'>
-								<div className='post text-center'>
-									<div className='space40'></div>
-									<div className='post-content text-left'>
-										<h1 className='post-title'>
-											<strong>{title[0]?.text}</strong>
-										</h1>
-										<RichText
-											render={description}
-											linkResolver={linkResolver}
-										/>
-										{/* META */}
-										<div className='meta'>
-											<span className='date'>
-												{dayjs(published_date).format('DD MMM, YYYY')}
-											</span>
-											<span className='views text-lowercase'>
-												{views} Views
-											</span>
-										</div>
-										<figure className='rounded'>
-											<img
-												src={featured_image?.url}
-												alt={featured_image?.alt}
-											/>
-										</figure>
-										<div className='space40'></div>
-										{/* ===== BLOG START ===== */}
+  /* ===== COUNT & UPDATE NO. OF VIEWS ===== */
+  // const [views, setViews] = useState(0);
+  // useEffect(() => {
+  //   countapi.hit("rigbiswas.com", uid).then((result) => {
+  //     result?.value && setViews(result.value);
+  //   });
+  // }, [uid]);
+  /* ===== END ===== */
+  return (
+    <section>
+      <div className="wrapper light-wrapper">
+        <div className="container inner pt-60">
+          <div className="row">
+            <div className="col-md-8 mx-auto">
+              <div className="blog classic-view">
+                <div className="post text-center">
+                  <div className="space40"></div>
+                  <div className="post-content text-left">
+                    <h1 className="post-title">
+                      <strong>{title[0]?.text}</strong>
+                    </h1>
+                    <RichText
+                      render={description}
+                      linkResolver={linkResolver}
+                    />
+                    {/* META */}
+                    <div className="meta">
+                      <span className="date">
+                        {dayjs(published_date).format("DD MMM, YYYY")}
+                      </span>
+                      {/* <span className="views text-lowercase">
+                        {views} Views
+                      </span> */}
+                    </div>
+                    <figure className="rounded">
+                      <img
+                        src={featured_image?.url}
+                        alt={featured_image?.alt}
+                      />
+                    </figure>
+                    <div className="space40"></div>
+                    {/* ===== BLOG START ===== */}
 
-										{blog_post_content?.map((data, index) => (
-											<ContentBox key={index} data={data} />
-										))}
+                    {blog_post_content?.map((data, index) => (
+                      <ContentBox key={index} data={data} />
+                    ))}
 
-										{/* <ul className='social social-mute social-s text-center'>
+                    {/* <ul className='social social-mute social-s text-center'>
 											<li>
 												<a href='#'>
 													<i className='fa fa-facebook'></i>
@@ -84,35 +84,35 @@ const BlogPostSection = ({ data, uid }) => {
 												</a>
 											</li>
 										</ul> */}
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 const ContentBox = ({ data }) => {
-	if (data.type == 'image') {
-		return (
-			<figure className={`figure position-relative overflow-hidden rounded`}>
-				<img
-					data-src={`${data?.url}?&w=750`}
-					className='img-fluid w-100 lozad'
-					alt={data?.alt}
-				/>
-				<div className='wall position-absolute w-100 h-100'></div>
-				<figcaption className='figure-caption position-absolute w-100 text-light px-2 text-nowrap overflow-auto'>
-					{data?.copyright ? `Image Source: ${data?.copyright}` : ''}
-				</figcaption>
-			</figure>
-		);
-	} else if (data?.type == 'paragraph' && data?.text == '') {
-		return '';
-	} else return <RichText render={[data]} linkResolver={linkResolver} />;
+  if (data.type == "image") {
+    return (
+      <figure className={`figure position-relative overflow-hidden rounded`}>
+        <img
+          data-src={`${data?.url}?&w=750`}
+          className="img-fluid w-100 lozad"
+          alt={data?.alt}
+        />
+        <div className="wall position-absolute w-100 h-100"></div>
+        <figcaption className="figure-caption position-absolute w-100 text-light px-2 text-nowrap overflow-auto">
+          {data?.copyright ? `Image Source: ${data?.copyright}` : ""}
+        </figcaption>
+      </figure>
+    );
+  } else if (data?.type == "paragraph" && data?.text == "") {
+    return "";
+  } else return <RichText render={[data]} linkResolver={linkResolver} />;
 };
 
 export default BlogPostSection;
--- components/blog/BlogsSection.js
+++ components/blog/BlogsSection.js
@@ -1,74 +1,74 @@
 /* eslint-disable @next/next/no-img-element */
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
-import dayjs from 'dayjs';
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import dayjs from "dayjs";
 dayjs().format();
-import countapi from 'countapi-js';
+// import countapi from "countapi-js";
 
 const BlogsSection = ({ posts }) => {
-	// console.log(posts);
-	return (
-		<div className='wrapper light-wrapper'>
-			<div className='container inner'>
-				<h2 className='section-title section-title-upper larger text-center'>
-					Latest Photography Blog Posts
-				</h2>
-				<p className='lead larger text-center'>
-					Here’s a log of our explorations, that we love to share with you!
-				</p>
-				<div className='space30'></div>
-				<div className='row isotope grid boxed list-view'>
-					{posts?.results?.map((data, index) => (
-						<BlogItem key={index} data={data} />
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  // console.log(posts);
+  return (
+    <div className="wrapper light-wrapper">
+      <div className="container inner">
+        <h2 className="section-title section-title-upper larger text-center">
+          Latest Photography Blog Posts
+        </h2>
+        <p className="lead larger text-center">
+          Here’s a log of our explorations, that we love to share with you!
+        </p>
+        <div className="space30"></div>
+        <div className="row isotope grid boxed list-view">
+          {posts?.results?.map((data, index) => (
+            <BlogItem key={index} data={data} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const BlogItem = ({ data }) => {
-	// console.log(data);
-	const { title, featured_image, published_date } = data?.data;
-	/* ===== COUNT & UPDATE NO. OF VIEWS ===== */
-	const [views, setViews] = useState(0);
-	useEffect(() => {
-		countapi.get('rigbiswas.com', data?.uid).then((result) => {
-			result?.value && setViews(result.value);
-		});
-	}, [data?.uid]);
-	/* ===== END ===== */
-	return (
-		<div className='item post grid-sizer col-md-6 col-lg-4 paper print'>
-			<figure className='overlay overlay1 mb-15 rounded'>
-				<Link href={`/blog/${data?.uid}`}>
-					<a>
-						<img
-							data-src={featured_image?.thumb?.url}
-							alt={featured_image?.alt}
-							className='lozad'
-						/>
-					</a>
-				</Link>
-			</figure>
-			<h4 className='post-title mb-10 font-weight-normal'>
-				<Link href={`/blog/${data?.uid}`}>
-					<a>{title?.[0]?.text}</a>
-				</Link>
-			</h4>
-			<div className='meta mb-15'>
-				<span className='date'>
-					{dayjs(published_date).format('DD MMM, YYYY')}
-				</span>
-				<span className='views text-lowercase'>
-					<span>{views}</span> views
-				</span>
-				{/* <span className='category'>
+  // console.log(data);
+  const { title, featured_image, published_date } = data?.data;
+  /* ===== COUNT & UPDATE NO. OF VIEWS ===== */
+  // const [views, setViews] = useState(0);
+  // useEffect(() => {
+  // 	countapi.get('rigbiswas.com', data?.uid).then((result) => {
+  // 		result?.value && setViews(result.value);
+  // 	});
+  // }, [data?.uid]);
+  /* ===== END ===== */
+  return (
+    <div className="item post grid-sizer col-md-6 col-lg-4 paper print">
+      <figure className="overlay overlay1 mb-15 rounded">
+        <Link href={`/blog/${data?.uid}`}>
+          <a>
+            <img
+              data-src={featured_image?.thumb?.url}
+              alt={featured_image?.alt}
+              className="lozad"
+            />
+          </a>
+        </Link>
+      </figure>
+      <h4 className="post-title mb-10 font-weight-normal">
+        <Link href={`/blog/${data?.uid}`}>
+          <a>{title?.[0]?.text}</a>
+        </Link>
+      </h4>
+      <div className="meta mb-15">
+        <span className="date">
+          {dayjs(published_date).format("DD MMM, YYYY")}
+        </span>
+        {/* <span className="views text-lowercase">
+          <span>{views}</span> views
+        </span> */}
+        {/* <span className='category'>
 					<a href='#'>Perspective</a>
 				</span> */}
-			</div>
-		</div>
-	);
+      </div>
+    </div>
+  );
 };
 
 export default BlogsSection;
