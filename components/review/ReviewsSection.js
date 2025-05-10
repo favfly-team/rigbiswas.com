36b259d2fadfd9e7a4b40b7807c618d1f8265956
FavFly Dev <favfly.dev@gmail.com>
2025-05-10 19:03:11 +0530

    Enhance ReviewsSection component by adding conditional rendering for heading and description, and improve layout styling for review items.

--- components/review/ReviewsSection.js
+++ components/review/ReviewsSection.js
@@ -4,12 +4,29 @@ import { linkResolver } from "../../prismic-configuration";
 
 /* eslint-disable @next/next/no-img-element */
 const ReviewsSection = ({ slice }) => {
-  // console.log(slice);
+  const { heading, description1 } = slice?.primary;
 
   return (
     <div className="wrapper light-wrapper">
       <div className="container inner">
-        <div className="row">
+        {heading?.[0]?.text && (
+          <>
+            <h2 className="section-title section-title-upper larger text-center">
+              {heading?.[0]?.text}
+            </h2>
+            <div className="lead larger text-center">
+              <RichText render={description1} linkResolver={linkResolver} />
+            </div>
+            <div className="space30"></div>
+          </>
+        )}
+
+        <div
+          className="row justify-content-center"
+          style={{
+            rowGap: "30px",
+          }}
+        >
           {slice?.items?.map((item, index) => (
             <ReviewItem key={index} data={item} />
           ))}
@@ -26,17 +43,21 @@ const ReviewItem = ({ data }) => {
     <div className="col-lg-4">
       <div
         onClick={() => setIsOpen(!isOpen)}
-        className={`cbp-item rounded overflow-hidden ${
+        className={`cbp-item m-0 h-100 rounded overflow-hidden ${
           isOpen ? "" : "review"
-        }`}>
-        <div className="bg-white shadow">
-          <img
-            key={image.url}
-            data-src={image.url}
-            alt={image.alt}
-            className="w-100 lozad"
-          />
-          <div className="mt-20 p-4">
+        }`}
+      >
+        <div className="bg-white shadow h-100">
+          {image.url && (
+            <img
+              key={image.url}
+              data-src={image.url}
+              alt={image.alt}
+              className="w-100 lozad mb-20 "
+            />
+          )}
+
+          <div className="p-4">
             <blockquote className="icon icon-left">
               <RichText render={review} linkResolver={linkResolver} />
               <footer className="blockquote-footer">{name?.[0]?.text}</footer>
